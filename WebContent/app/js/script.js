1a767b16e99a6a70f6d01701a36cb13908cbbe17
Mohd Shariq <mdshariq614@gmail.com>
2021-09-20 03:49:44 +0530

    Add hamburger menu animation and overlay

--- WebContent/app/js/script.js
+++ WebContent/app/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector("#header");
+const overlay = document.querySelector(".overlay");
+btnHamburger.addEventListener("click",function(){
+    header.classList.toggle("open");
+    if(overlay.classList.contains("fade-in")){
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    }else{
+        overlay.classList.remove("fade-out");
+        overlay.classList.add("fade-in");
+    }
+});
